c9902fe38608fade60643ec30a23e6c52a57c008
Cyphall <cyphallbraus@gmail.com>
2020-12-29 18:27:18 +0100

    Renamed a few horribly named variable

--- src/MountainGenerator.cpp
+++ src/MountainGenerator.cpp
@@ -1,36 +1,36 @@
 #include "MountainGenerator.h"
 
-BlockContainer MountainGenerator::generate(int x, int y, int z)
+BlockContainer MountainGenerator::generate(int chunkX, int chunkY, int chunkZ)
 {
 	BlockContainer blockContainer;
 	
-	for (int heightY = 0; heightY < 16; heightY++)
+	for (int blockZ = 0; blockZ < 16; blockZ++)
 	{
-		for (int heightX = 0; heightX < 16; heightX++)
+		for (int blockX = 0; blockX < 16; blockX++)
 		{
-			float surfaceHeightNoise = _noise.getNoise(x + heightX / 16.0f, z + heightY / 16.0f, 6, 12, 0.4, 0.5);
+			float surfaceHeightNoise = _noise.getNoise(chunkX + blockX / 16.0f, chunkZ + blockZ / 16.0f, 6, 12, 0.4, 0.5);
 			
 			int surfaceHeight = MIN_HEIGHT + static_cast<int>(surfaceHeightNoise * (MAX_HEIGHT - MIN_HEIGHT));
 			
-			for (int height = 0; height < 16; height++)
+			for (int blockY = 0; blockY < 16; blockY++)
 			{
-				int worldHeight = y * 16 + height;
+				int worldSpaceBlockY = chunkY * 16 + blockY;
 				
-				if (worldHeight < surfaceHeight - 4)
+				if (worldSpaceBlockY < surfaceHeight - 4)
 				{
-					blockContainer.setBlock(heightX, height, heightY, STONE);
+					blockContainer.setBlock(blockX, blockY, blockZ, STONE);
 				}
-				else if (worldHeight < surfaceHeight - 1)
+				else if (worldSpaceBlockY < surfaceHeight - 1)
 				{
-					blockContainer.setBlock(heightX, height, heightY, DIRT);
+					blockContainer.setBlock(blockX, blockY, blockZ, DIRT);
 				}
-				else if (worldHeight < surfaceHeight)
+				else if (worldSpaceBlockY < surfaceHeight)
 				{
-					blockContainer.setBlock(heightX, height, heightY, GRASS);
+					blockContainer.setBlock(blockX, blockY, blockZ, GRASS);
 				}
 				else
 				{
-					blockContainer.setBlock(heightX, height, heightY, AIR);
+					blockContainer.setBlock(blockX, blockY, blockZ, AIR);
 				}
 			}
 		}
