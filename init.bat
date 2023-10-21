596a88f848b0b6f9d93cff9df82fe01c496ecb62
Cyphall <cyphallbraus@gmail.com>
2023-10-21 15:47:10 +0200

    remove unused files from vcpkg

--- init.bat
+++ init.bat
@@ -6,5 +6,8 @@ git checkout 66045de4dcc5da3d1029c02b606307f5951dcb22
 call bootstrap-vcpkg.bat
 vcpkg install glfw3 glm stb magic-enum
 rmdir /s /q .git
+rmdir /s /q buildtrees
+rmdir /s /q downloads
+rmdir /s /q packages
 cd ..
 pause
